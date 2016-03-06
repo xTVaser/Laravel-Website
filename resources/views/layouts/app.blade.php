3a6c834f3805230ef1b3cc2eb019f65b1f74c01b
Tyler <xtvaser@gmail.com>
2016-03-06 17:43:05 -0500

    Added an edit profile button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,11 +59,12 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                Logged in as: <b>{{ Auth::user()->email }}</b> <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{ url('/editprofile') }}"><i class="fa fa-btn fa-pencil-square-o"></i>Edit Profile</a></li>
+                                <li><a href="{{ url('/logout') }} "><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
                     @endif
