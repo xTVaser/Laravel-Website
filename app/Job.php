86073c90375476478c49651b2120e1b35877b240
Fabio Campioni <fabcampioni@gmail.com>
2016-03-06 19:13:53 -0500

    Set mass assignable fields on Job model

--- app/Job.php
+++ app/Job.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //Gets the applications associated with this job
-    public function getApplications() {
-        return $this->hasMany('Application');
-    }
+  //Fields that can be filled automatically
+  protected $fillable = [
+    'title', 'description', 'qualifications', 'salary', 'start_date', 'closing_date', 'job_type',
+  ];
+
+  //Gets the applications associated with this job
+  public function getApplications() {
+    return $this->hasMany('Application');
+  }
 }
