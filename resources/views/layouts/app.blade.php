342f765d07c08bea206d082dc6510ba5036e79db
Robert Brown <robrownn@gmail.com>
2016-03-11 20:01:20 -0500

    Created custom CSS file in public/css. Fixed favicon by making a favicon folder and converting it to a PNG.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,16 @@
 
     <title>E.A.R.S.</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="favicon/favicon.png">
+
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 
     <style>
         body {
