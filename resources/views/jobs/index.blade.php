07835b621e64c56c855f70392e92f451cd4cc719
Robert Brown <robrownn@gmail.com>
2016-03-22 19:29:42 -0400

    Improved job listing page. Began work on improving my applications page.

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -6,10 +6,25 @@
     <h1>Job Listings</h1>
   </div>
 
-  @foreach($jobs as $job)
-    <h2>{{ $job->title }}</h2>
-    <p>{{ $job->description }}</p>
-    <p>{{ $job->salary }}</p>
-  @endforeach
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading" id="panelHeader">Jobs</div>
+        <div class="panel-body">
+          <!-- if jobs exist do this -->
+          @foreach($jobs as $job)
+            <h2>{{ $job->title }}</h2>
+            <p>{{ $job->description }}</p>
+            <p>{{ $job->salary }}</p>
+          @endforeach
+          <!-- else do this:
+            <h2>No Jobs available! Check back soon!</h2>
+          -->
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
 @endsection
