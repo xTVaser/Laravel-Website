eda88cec8a7d9b67466285a86b4887458fb2eb8a
Tyler Wilding <xtvaser@gmail.com>
2016-03-22 19:24:43 -0400

    Updated some of the CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,700|Arimo:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Rubik:400,700' rel='stylesheet' type='text/css'>
 
     <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.3/less.min.js"></script>
@@ -23,7 +24,7 @@
 
     <style>
         body {
-            font-family: 'Ubuntu';
+            font-family: 'Open Sans', sans-serif;
         }
 
         .fa-btn {
