e0241882bd992f76dd41674f8929b75bd539e6c4
Robert Brown <robrownn@gmail.com>
2016-03-22 14:43:31 -0400

    Added content to Create Account page. Now requires logic to actually create an account

--- resources/views/admin/createAccount.blade.php
+++ resources/views/admin/createAccount.blade.php
@@ -5,10 +5,40 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Create an Account</div>
+                <div class="panel-heading" id="panelHeader">Create an Account</div>
 
                 <div class="panel-body">
-                    You are logged in!
+
+                  {!! Form::open() !!}
+                  {!! csrf_field() !!}
+
+                  <div class="form-group">
+                    {!! Form::label('Email') !!}
+                    {!! Form::email('Email', null, ['class' => 'form-control']) !!}
+                  </div>
+
+
+                  <!-- 0 = default user, 1 = faculty, 2 = committee member, 3 = committee chair -->
+                  <div class="form-group">
+                    {!! Form::label('Account Type:') !!}
+
+                    <br>
+
+                    {!! Form::label('Default') !!}
+                    {!! Form::radio('flag', 0, ['class' => 'form-control']) !!}
+
+                    {!! Form::label('Faculty') !!}
+                    {!! Form::radio('flag', 1, ['class' => 'form-control']) !!}
+
+                    {!! Form::label('Committee Member') !!}
+                    {!! Form::radio('flag', 2, ['class' => 'form-control']) !!}
+                  </div>
+
+                  <div class="form-group">
+                    {!! Form::label('Submit') !!}
+                    {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
+                  </div>
+
                 </div>
             </div>
         </div>
