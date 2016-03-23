f53db88841d1b101c389ced3b113a6f0ff3c607a
Robert Brown <robrownn@gmail.com>
2016-03-23 14:35:29 -0400

    Did work on things I forget what exactly.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Home Page</div>
+                <div class="panel-heading" id="panelHeader">Home Page</div>
 
                 <div class="panel-body">
                     This is the homepage, it is fairly useless at the moment!<br><br>
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,26 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row">
-<<<<<<< HEAD
-        <div class="col-sm-2">
-            <div class="panel panel-default">               <!-- Put Echo Tags here once merged with fabs code -->
-                <div class="panel-heading" id="panelHeader">$profile->first_name</div>
-
-                <div class="panel-body">
-                    <button class="btn btn-primary"><a href="linkedinpagehere">My LinkedIn Page</a></button>
-=======
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading" id="panelHeader">Profile: {{ $profile->first_name }}</div>
-
-                <div class="panel-body">
-                    You are logged in!
-
-
->>>>>>> dev
-                </div>
-            </div>
+      <div class="col-sm-2">
+        <div class="panel panel-default">
+          <div class="panel-heading" id="panelHeader">Profile: {{ $profile->first_name }}</div>
+          <div class="panel-body">
+            <button class="btn btn-primary"><a href="linkedinpagehere">My LinkedIn Page</a></button>
+          </div>
         </div>
+      </div>
     </div>
 
   <div class="row">
