1d74e29a2bafb0fb039055e3ac994f4ebed2c2f0
Tyler Wilding <xtvaser@gmail.com>
2016-03-23 23:38:58 -0400

    Ye

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -4,7 +4,6 @@
 
 <div class="container">
     <div class="row">
-<<<<<<< HEAD
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading" id="panelHeader">{{ $profile->first_name }}</div>
@@ -14,22 +13,7 @@
 
 
                     <button class="btn btn-primary"><a href="linkedinpagehere">My LinkedIn Page</a></button>
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading" id="panelHeader">Profile: {{ $profile->first_name }}</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-=======
-      <div class="col-sm-2">
-        <div class="panel panel-default">
-          <div class="panel-heading" id="panelHeader">Profile: {{ $profile->first_name }}</div>
-          <div class="panel-body">
-            <button class="btn btn-primary"><a href="linkedinpagehere">My LinkedIn Page</a></button>
-          </div>
->>>>>>> html
+                <div class="col-md-10 col-md-offset-1">
         </div>
       </div>
     </div>
