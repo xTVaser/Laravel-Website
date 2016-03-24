0bb48a6f4c91ae037e9fb0100b5b24b7a9475b28
Robert Brown <robrownn@gmail.com>
2016-03-24 14:19:16 -0400

    Added file upload to apply page. Hidden field made for Will, just gotta add da khode.

--- resources/views/applications/apply.blade.php
+++ resources/views/applications/apply.blade.php
@@ -11,10 +11,21 @@
 
                         {!! Form::open() !!}
                         {!! csrf_field() !!}
+                        <div class="form-group">
+                          {!! Form::file('resume', null, ['class' => 'btn btn-primary form-control']) !!}
+                        </div>
+                        
+                        <div class="form-group">
+                          <!-- this is going to be a hidden field that takes in the job object.
+                          when the application is submitted this is sent with it so that the system knows what job the person is applying for.
+                          write da khode will :D -->
+                        </div>
 
                         <div class="form-group">
                           {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
                         </div>
+
+
 
                         {!! Form:close() !!}
                 </div>
