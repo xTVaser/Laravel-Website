1a81ca9b9aa0ae5673ba19501ae6bc21918d46bd
Robert Brown <robrownn@gmail.com>
2016-03-27 13:33:51 -0400

    Added cover letter file upload to apply page as well as fields from the edit profile page just in case a user wants to change things before they upload.

--- resources/views/applications/apply.blade.php
+++ resources/views/applications/apply.blade.php
@@ -11,10 +11,37 @@
 
                         {!! Form::open() !!}
                         {!! csrf_field() !!}
+
                         <div class="form-group">
+                                {!! Form::label('First Name') !!}
+                                {!! Form::text('first_name', $profile->first_name, ['class' => 'form-control', 'placeholder' => 'John']) !!}
+                        </div>
+
+                        <div class="form-group">
+                                {!! Form::label('Middle Name') !!}
+                                {!! Form::text('middle_name', null, ['class' => 'form-control', 'placeholder' => 'M.']) !!}
+                        </div>
+
+                        <div class="form-group">
+                                {!! Form::label('Last Name') !!}
+                                {!! Form::text('last_name', null, ['class' => 'form-control', 'placeholder' => 'Smith']) !!}
+                        </div>
+
+                        <div class="form-group">
+                                {!! Form::label('Contact Email') !!}
+                                {!! Form::email('contact_email', null, ['class' => 'form-control', 'placeholder' => 'jsmith@algomau.ca']) !!}
+                        </div>
+
+                        <div class="form-group">
+                          {!! Form::label('Resume') !!}
                           {!! Form::file('resume', null, ['class' => 'btn btn-primary form-control']) !!}
                         </div>
-                        
+
+                        <div class="form-group">
+                          {!! Form::label('Cover Letter') !!}
+                          {!! Form::file('cover-letter', null, ['class' => 'btn btn-primary form-control']) !!}
+                        </div>
+
                         <div class="form-group">
                           <!-- this is going to be a hidden field that takes in the job object.
                           when the application is submitted this is sent with it so that the system knows what job the person is applying for.
