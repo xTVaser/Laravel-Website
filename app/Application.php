49a375584d99af78f77778d111e80fd63cd3db36
Tyler Wilding <xtvaser@gmail.com>
2016-04-01 20:32:42 -0400

    Improved application listing page, comments will now show their authors name and the date they were created and possibly modified

--- app/Application.php
+++ app/Application.php
@@ -30,7 +30,14 @@ class Application extends Model
   public static function sqlComments($id) {
 
           return $comments = DB::table('comments')
-                                ->select('*')
+                                ->join('profiles', 'comments.author_id', '=', 'profiles.user_id')
+                                ->select(   'comments.id as id',
+                                            'comments.application_id as application_id',
+                                            'comments.author_id as author_id',
+                                            'comments.body as body',
+                                            'comments.created_at',
+                                            'comments.updated_at',
+                                            'profiles.first_name as author_name')
                                 ->where('application_id', '=', $id)
                                 ->distinct()
                                 ->get();
@@ -39,93 +46,93 @@ class Application extends Model
   public static function joinJobsAndApplicationsAndProfiles() {
 
           return $apps = DB::table('applications')
-                    ->join('jobs', 'applications.job_id', '=', 'jobs.id')
-                    ->join('profiles', 'applications.user_id', '=', 'profiles.user_id')
-                    ->select(   'applications.id as app_id',
-                                'status',
-                                'applications.created_at as app_created_at',
-                                'applications.updated_at as app_updated_at',
-                                'job_id',
-                                'applications.user_id',
-                                'title',
-                                'jobs.description',
-                                'qualifications',
-                                'salary',
-                                'start_date',
-                                'closing_date',
-                                'job_type',
-                                'jobs.created_at as job_created_at',
-                                'first_name',
-                                'middle_name',
-                                'last_name',
-                                'contact_email')
-                    ->distinct()
-                    ->get();
+                            ->join('jobs', 'applications.job_id', '=', 'jobs.id')
+                            ->join('profiles', 'applications.user_id', '=', 'profiles.user_id')
+                            ->select(   'applications.id as app_id',
+                                        'status',
+                                        'applications.created_at as app_created_at',
+                                        'applications.updated_at as app_updated_at',
+                                        'job_id',
+                                        'applications.user_id',
+                                        'title',
+                                        'jobs.description',
+                                        'qualifications',
+                                        'salary',
+                                        'start_date',
+                                        'closing_date',
+                                        'job_type',
+                                        'jobs.created_at as job_created_at',
+                                        'first_name',
+                                        'middle_name',
+                                        'last_name',
+                                        'contact_email')
+                            ->distinct()
+                            ->get();
   }
 
   public static function joinJobsAndApplications() {
 
           return $apps = DB::table('applications')
-                    ->leftJoin('jobs', 'job_id', '=', 'jobs.id')
-                    ->select(   'applications.id as app_id',
-                                'status',
-                                'applications.created_at as app_created_at',
-                                'applications.updated_at as app_updated_at',
-                                'job_id',
-                                'user_id',
-                                'title',
-                                'description',
-                                'qualifications',
-                                'salary',
-                                'start_date',
-                                'closing_date',
-                                'job_type',
-                                'jobs.created_at as job_created_at')
-                    ->distinct()
-                    ->get();
+                            ->leftJoin('jobs', 'job_id', '=', 'jobs.id')
+                            ->select(   'applications.id as app_id',
+                                        'status',
+                                        'applications.created_at as app_created_at',
+                                        'applications.updated_at as app_updated_at',
+                                        'job_id',
+                                        'user_id',
+                                        'title',
+                                        'description',
+                                        'qualifications',
+                                        'salary',
+                                        'start_date',
+                                        'closing_date',
+                                        'job_type',
+                                        'jobs.created_at as job_created_at')
+                            ->distinct()
+                            ->get();
   }
   public static function joinJobsAndApplicationsOnID($id) {
 
           return $apps = DB::table('applications')
-                    ->leftJoin('jobs', 'job_id', '=', 'jobs.id')
-                    ->select(   'applications.id as app_id',
-                                'status',
-                                'applications.created_at as app_created_at',
-                                'applications.updated_at as app_updated_at',
-                                'job_id',
-                                'user_id',
-                                'title',
-                                'description',
-                                'qualifications',
-                                'salary',
-                                'start_date',
-                                'closing_date',
-                                'job_type',
-                                'jobs.created_at as job_created_at')
-                    ->where('applications.id', '=', $id)
-                    ->first();
+                            ->leftJoin('jobs', 'job_id', '=', 'jobs.id')
+                            ->select(   'applications.id as app_id',
+                                        'status',
+                                        'applications.created_at as app_created_at',
+                                        'applications.updated_at as app_updated_at',
+                                        'job_id',
+                                        'user_id',
+                                        'title',
+                                        'description',
+                                        'qualifications',
+                                        'salary',
+                                        'start_date',
+                                        'closing_date',
+                                        'job_type',
+                                        'jobs.created_at as job_created_at')
+                            ->where('applications.id', '=', $id)
+                            ->first();
   }
 
   public static function joinJobsAndApplicationsOnUserID($id) {
 
           return $apps = DB::table('applications')
-                    ->leftJoin('jobs', 'job_id', '=', 'jobs.id')
-                    ->select(   'applications.id as app_id',
-                                'status',
-                                'applications.created_at as app_created_at',
-                                'applications.updated_at as app_updated_at',
-                                'job_id',
-                                'user_id',
-                                'title',
-                                'description',
-                                'qualifications',
-                                'salary',
-                                'start_date',
-                                'closing_date',
-                                'job_type',
-                                'jobs.created_at as job_created_at')
-                    ->distinct()
-                    ->where('user_id', '=', $id)
-                    ->get();
+                            ->leftJoin('jobs', 'job_id', '=', 'jobs.id')
+                            ->select(   'applications.id as app_id',
+                                        'status',
+                                        'applications.created_at as app_created_at',
+                                        'applications.updated_at as app_updated_at',
+                                        'job_id',
+                                        'user_id',
+                                        'title',
+                                        'description',
+                                        'qualifications',
+                                        'salary',
+                                        'start_date',
+                                        'closing_date',
+                                        'job_type',
+                                        'jobs.created_at as job_created_at')
+                            ->distinct()
+                            ->where('user_id', '=', $id)
+                            ->get();
   }
 }
--- resources/views/applications/application.blade.php
+++ resources/views/applications/application.blade.php
@@ -6,9 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading" id="panelHeader">Application: {{ $application->title }}</div>
-                        <div class="panel-body">
-
-
+                <div class="panel-body">
 
                     <a href="{{ url('/profile/')}}/{{ $profile->user_id }}">{{ $profile->first_name }} {{ $profile->middle_name }} {{ $profile->last_name }} </a>
                     <p><i>Originally applied on: {{ $application->app_created_at}} </i> </p>
@@ -21,59 +19,63 @@
 
                     @if( $application->status == 'Pending' )
                         <h2 style="color: #c4800c">Application Pending</h2>
-                        @else
+                    @else
                         <h2 style="color: #427d22">Application Approved</h2>
-                        @endif
+                    @endif
 
                     @if($application->closing_date < (\Carbon\Carbon::now()))
-                      <div class="form-group">
-                              <input type="submit" name="approve" value="Approve Applicant" class="btn btn-success" >
-                              <input type="submit" name="deny" value="Deny Applicant" class="btn btn-danger" >
-                      </div>
+                    <div class="form-group">
+                        <input type="submit" name="approve" value="Approve Applicant" class="btn btn-success" >
+                        <input type="submit" name="deny" value="Deny Applicant" class="btn btn-danger" >
+                    </div>
+
                     @else
-                    <b>Applications Still Open!</b><br><br>
+                        <b>Applications Still Open!</b><br><br>
                     @endif
 
 
                     <div class="form-group">
-                          <input type="submit" name="dl_resume" value="Download Resume" class="btn btn-primary">
-                          <input type="submit" name="dl_coverletter" value="Download Cover Letter" class="btn btn-primary">
+                        <input type="submit" name="dl_resume" value="Download Resume" class="btn btn-primary">
+                        <input type="submit" name="dl_coverletter" value="Download Cover Letter" class="btn btn-primary">
                     </div>
-
 
                     <div class="form-group">
-                            {!! Form::textarea('commentText', null, ['class' => 'form-control', 'placeholder' => 'Comment...']) !!}
-                    </div>
-                    <div class="form-group">
-                            <input type="submit" name="post_comment" value="Post Comment" class="btn btn-info">
+                        {!! Form::textarea('commentText', null, ['class' => 'form-control', 'placeholder' => 'Comment...']) !!}
                     </div>
 
+                    <div class="form-group">
+                        <input type="submit" name="post_comment" value="Post Comment" class="btn btn-info">
+                    </div>
 
                     {!! Form::close() !!}
 
                     @forelse($comments as $comment)
-
-                    <p style="text-indent: 1em;">
-                      {{ $comment->body }}
-                      {!! Form::open() !!}
-                      {!! csrf_field() !!}
-                      <div id="comment{{ $comment->id }}">
-                      {!! Form::hidden('comment_id', $comment->id) !!}
-                      {{-- Only if the comment matches the user id, aka it's their comment --}}
-                      @if( $comment->author_id != $currentUser )
-                      <input id="replyButton{{ $comment->id }}" type="button" value="Reply" class="btn btn-info" onclick="clickReply({{ $comment->id }});" />
-                      @else
-                      <input id="editButton{{ $comment-> id }}" type="button" value="Edit" class="btn btn-warning" onclick="clickEdit({{ $comment->id }}, '{{ $comment->body }}');" />
-                      @endif
+                        <div class="col-md-10" style="padding-bottom: 4em">
+                            <p style="text-indent: 1em;">{{ $comment->body }}</p>
+                            <i>{{ $comment->author_name }} - Created At: {{ $comment->created_at }}</i>
+                            @if( $comment->created_at != $comment->updated_at )
+                            <b><i> - Edited at: {{ $comment->updated_at }}</b></i>
+                            @endif
                         </div>
-                      {!! Form::close() !!}
-                    </p>
 
-
+                        {!! Form::open() !!}
+                        {!! csrf_field() !!}
+                            <div id="comment{{ $comment->id }}" style="padding-bottom: 10px">
+                                {!! Form::hidden('comment_id', $comment->id) !!}
+                                {{-- Only if the comment matches the user id, aka it's their comment --}}
+                                <div class="col-md-2">
+                                @if( $comment->author_id != $currentUser )
+                                    <input id="replyButton{{ $comment->id }}" type="button" value="Reply" class="btn btn-info" onclick="clickReply({{ $comment->id }});" />
+                                @else
+                                    <input id="editButton{{ $comment-> id }}" type="button" value="Edit" class="btn btn-warning" onclick="clickEdit({{ $comment->id }}, '{{ $comment->body }}');" />
+                                @endif
+                                </div>
+                            </div>
+                        {!! Form::close() !!}
                     @empty
-                    <p>No Comments</p>
+                        <p>No Comments</p>
                     @endforelse
-            </div>
+                    </div>
                 </div>
             </div>
         </div>
